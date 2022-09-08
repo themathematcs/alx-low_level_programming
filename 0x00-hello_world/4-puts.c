37de0d3b5507bc3f4078e1aaa560a4782ad9f3dc
themathematcs <chriznganga69@gmail.com>
2022-09-08 08:56:06 -0700

    tired

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
-
+/**
+ * trying the code
+ * returning 0
+ * /
 int main(void)
 {
 
        puts("\"Programming is like building a multilingual puzzle");
-      
        return (0);
 }
