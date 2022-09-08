9166448d6bc0b3df1d3e1d55c996f3da0a3f687d
themathematcs <chriznganga69@gmail.com>
2022-09-08 14:40:46 -0700

    one error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 
-printf("size of a char: %d byte(s)\n", sizeof(char));
-printf("size of an int %d byte(s)\n", sizeof(int));
-printf("size of a long int %d byte(s)\n" sizeof(long int));
-printf("size of a long long int %d byte(s)\n" sizeof(long long int));
-printf("size of a float %d bytes(s)\n" sizeof(float));
+printf("size of a char: %! byte(s)\n", sizeof(char));
+printf("size of an int %! byte(s)\n", sizeof(int));
+printf("size of a long int %! byte(s)\n" sizeof(long int));
+printf("size of a long long int %! byte(s)\n" sizeof(long long int));
+printf("size of a float %! bytes(s)\n" sizeof(float));
 return (0);
 
 }
