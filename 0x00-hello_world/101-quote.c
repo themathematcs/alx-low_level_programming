6b57146b1f52399e2f9485415e2b0fc5c01cbea1
themathematcs <chriznganga69@gmail.com>
2022-09-08 14:24:12 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point,
+ *
+ * Return: 0 when executed succefliy.
+ */
+
+int main(void)
+{
+	
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+return (1);
+
+}
