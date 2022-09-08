44566bbcfaf0a7a1daf38ce628cf681247ac278b
themathematcs <chriznganga69@gmail.com>
2022-09-08 09:20:20 -0700

    betty errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main: prints "Programming is like building a multilingual puzzle
+ * return: 0 is if is excecuted correctly, otherwise non zero
+ */
 
 int main(void)
 {
 
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 
 }
