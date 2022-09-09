32b6f954671fad456d58953c918cf9bc531bb166
themathematcs <chriznganga69@gmail.com>
2022-09-09 12:44:32 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
- * main -main entry point
- * return:0
+ * main -prints all digits of numbers 
+ * Return: 0
  */
 int main(void)
 {
 int n;
-for (n = 0; n < 10; n++)
+for (n = 0; n <= 10; n++)
+{
 putchar(n);
-putchar("\n");
+}
+putchar('\n');
 return (0);
 }
