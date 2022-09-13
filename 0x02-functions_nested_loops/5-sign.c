9f38af353ecf80be62e56e25d77490d6eea0c75a
themathematcs <chriznganga69@gmail.com>
2022-09-13 12:28:22 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,12 +10,16 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar(48);
+_putchar('+');
 return (1);
 }
-else if (n < 0)
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
 {
 _putchar('_');
-}
 return (-1);
 }
