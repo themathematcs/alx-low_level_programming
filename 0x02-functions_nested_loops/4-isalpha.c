79ec9dcf93ce362b508f279e940fb90da70f3b25
themathematcs <chriznganga69@gmail.com>
2022-09-13 12:06:05 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha -> checks if number is lower or uper case
+ * @c: character argument
+ * Return: 1 if c is a letter, 0 if otherwise
+ */
+int _isalpha(int c)
+{
+return ((c >= 'a' && <= 'z') || (c >= 'A' && c <= 'Z'));
+}
