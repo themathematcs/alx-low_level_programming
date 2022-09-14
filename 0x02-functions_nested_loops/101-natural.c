46d3cd066daa45d8848d9fd5a6753b82c8b4b999
themathematcs <chriznganga69@gmail.com>
2022-09-14 12:43:54 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * void ->  returns a value 0 if succesful
  * main -> computes and prints the sum of all the multiples
  * of 3 || 5 below 1024
  * Return:0
  */
-int void(main)
+
+int main(void)
 {
 unsigned long int sum3, sum5, sum;
 int i;
