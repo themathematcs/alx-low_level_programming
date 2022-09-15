0f4b59497fb6889ff6c015dbab17a55aa66bfee1
themathematcs <chriznganga69@gmail.com>
2022-09-15 12:21:04 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,6 +6,7 @@
 void print_most_numbers(void)
 	{
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 	if (i != 2 && i != 4)
@@ -13,6 +14,5 @@ void print_most_numbers(void)
 		_putchar(i + '0');
 	}
 	_putchar('\n');
-	}
 	return (0);
 	}
