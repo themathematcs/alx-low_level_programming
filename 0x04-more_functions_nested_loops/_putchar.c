81b717a136f8c5759242a1644bc95715c7246bfb
themathematcs <chriznganga69@gmail.com>
2022-09-15 04:22:29 -0700

    int _putchar(char c)

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,4 +1,5 @@
-#include <unistd.h>
+#include  <unistd.h>
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -7,6 +8,5 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
-{
-return (write(1, &c, 1));
 }
+return (write(1, &c, 1));
