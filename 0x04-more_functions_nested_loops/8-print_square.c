830e25243ecdb35573c84069984c05ec347faf0e
themathematcs <chriznganga69@gmail.com>
2022-09-15 16:07:36 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - prints square
+ * @size: argument
+ * Return:0
+ */
+void print_square(int size);
+{
+int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
