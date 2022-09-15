2922991eb6c81421be754f1d00243a4385921604
themathematcs <chriznganga69@gmail.com>
2022-09-15 16:38:47 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -> prints the numbers
+ * Return:0
+ */
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf(" Fizz");
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf(" Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i == 1)
+{
+printf("%d", i);
+}
+else
+{
+printf("%d", i);
+}
+}
+printf("\n");
+return (0);
+}
