0850191ead96218f9b64d48cf094d97ec74dc207
themathematcs <chriznganga69@gmail.com>
2022-09-15 10:07:49 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,10 @@
+#ifndef Main_H
+#define MAIN_H
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-_putchar(char c);
+#endif
