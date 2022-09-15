a364a3c583129c31c66063fa62d16eae2fd06161
themathematcs <chriznganga69@gmail.com>
2022-09-15 03:30:12 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers -> prints 1 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+int i;
+i = 0;
+
+for (i = 0; i < 10; i++)
+{
+_putchar(i);
+}
+return (0);
+}
