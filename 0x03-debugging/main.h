86e6b6df78b385912d026e62408f93ed07c40509
themathematcs <chriznganga69@gmail.com>
2022-09-15 02:59:54 -0700

    0-isupper.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int main(void);
+void positive_or_negative(int );
 
 #endif /* MAIN_H */
---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper -> check the code
+ * @c: character argument
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
