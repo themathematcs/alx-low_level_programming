aa01f3bfee8050bfa2bb89757ee524a87b6ba37a
themathematcs <chriznganga69@gmail.com>
2022-09-15 18:14:11 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,19 +5,20 @@
  */
 void print_number(int n)
 {
-unsigned int n1;
-if (n < 0)
-{
-n1 = -n;
-_putchar('-');
-}
-else
-{
-n1 = n;
-}
-if (n1 / 10)
-{
-print_numbers(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+	unsigned int n1;
+
+	if (n < 0)
+	{	
+	n1 = -n;
+	_putchar('-');
+	}
+	else
+	{
+	n1 = n;
+	}
+	if (n1 / 10)
+	{
+	print_numbers(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
