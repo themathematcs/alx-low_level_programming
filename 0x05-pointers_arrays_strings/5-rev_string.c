08a2f421db72606f314e614049b292a179523e0e
themathematcs <chriznganga69@gmail.com>
2022-09-19 11:52:17 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string -> reverses string in the function
+ * @s: character argument
+ */
+void rev_string(char *s)
+{
+char tmp;
+int i, len, len1;
+len = 0;
+len1 = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+len1 = len - 1;
+for (i = 0; i < len / 2; i++)
+{
+tmp = s[i];
+s[i] = s[len1];
+s[len--] = tmp;
+}
+}
