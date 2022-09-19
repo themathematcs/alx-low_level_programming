70a484323ba29743ee796d8c54c63366fdbcc8f9
themathematcs <chriznganga69@gmail.com>
2022-09-19 01:54:05 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int -> swaps the value of a and b
+ * @a: character argument to be swaped by b
+ * @b: character argument to be swaped by a
+ *
+ */
+void swap_int(int *a, int *b)
+{
+int *c;
+a = 98;
+b = 42;
+c = a;
+a = b;
+b = c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 void reset_to_98(int *n);
+wap_int(int *a, int *b);
 
 #endif
