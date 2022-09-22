97f32b95cff30e3faf812988cea4551d7a5cf341
themathematcs <chriznganga69@gmail.com>
2022-09-22 13:38:26 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,16 +1,28 @@
 #include "main.h"
 /**
- *
+ * cap_string -> capitalizes the strings provided
+ * @s: string to be capitalizes
+ * Return: (s)
  *
  */
-char *cap_string(char *m)
+char *cap_string(char *s)
 {
 int i, j;
-char spe[13] = {' ', '\t', '\n', ',', '.', '!','?', '"', '(', ')', '{', '}'};
+char spe[13] = {' ', '\t', '\n', ',', '.', '!', '?', '"', '(', ')', '{', '}'};
 for (i = 0; m[i] != '\0'; i++)
 {
-if (m[i] 
-
+if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+	s[i] -= 32;
+for (j = 0; j < 13; j++)
+{
+if (s[i] == spe[j])
+{
+if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+{
+s[i + 1] -= 32;
 }
-
+}
+}
+}
+return (s);
 }
