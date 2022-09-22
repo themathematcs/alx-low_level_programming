cbd10979ed358f897d50fe777f0d5cfaa194ae74
themathematcs <chriznganga69@gmail.com>
2022-09-22 13:24:31 -0700

    char *_strcat(char *dest, char *src);

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,26 @@
 #include "main.h"
 /**
  * _strcat -> concatinates two strings
- * @dest: character argument
- * @src: string add
- * Return: dest as return
+ * @dest: character argument to be used
+ * @src: character 2 to be used
+ * Return: dest when succesfull
+ *
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+int i, j;
+i = 0;
+j = 0;
 
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
+while (dest[i] != '\0')
+	i++;
+while (src[j] != '\0')
+{
+	des[i] = src[j];
+	j++;
+	i++;
+}
+dest[i] = '\0';
 
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
+return (dest);
 }
