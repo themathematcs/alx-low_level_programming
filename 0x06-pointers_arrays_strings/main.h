3cfb2e39288f277a02817d04aeb1c564da185b8a
themathematcs <chriznganga69@gmail.com>
2022-09-22 12:54:33 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
