4b55e116e7495a3f3acde1b20e16894d4aea4710
themathematcs <chriznganga69@gmail.com>
2022-09-22 14:44:15 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number -> prints a  number
+ * @n: integer
+ *
+ */
+void print_number(int n)
+{
+unsigned int n1;
+
+if (n < 0)
+{
+n1 = -n;
+_putchar('-');
+}
+else
+{
+n1 = n;
+}
+if (n1 / 10)
+print_numbers(n1 / 10);
+_putchar((n1 % 10) + '0');
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
 char *rot13(char *s);
+void print_number(int n);
 
 #endif
