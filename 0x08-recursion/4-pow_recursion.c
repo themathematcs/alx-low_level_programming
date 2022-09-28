9c03b27cc72c01baf4d7a9b4213f389506b789f0
themathematcs <chriznganga69@gmail.com>
2022-09-28 00:08:27 -0700

    raising the power of an intager

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion -> raises the power of x by y
+ * @x: intager arg
+ * @y: intager arg
+ *
+ * Return: after power up
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+	return (-1);
+return (x * _pow_recursion(x, y - 1));
+}
