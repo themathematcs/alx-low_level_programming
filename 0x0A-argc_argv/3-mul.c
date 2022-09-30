cf7a1299f58173187f4a23dba373ebcdf126e5d0
themathematcs <chriznganga69@gmail.com>
2022-09-30 11:32:09 -0700

    multiplying two arguments passed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,16 @@
  */
 int main(int argc, char *argv[])
 {
-int a, b, c;
+	int a, b, c;
 
-if (argc != 3)
-{
-puts("Error");
-return (1);
-}
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-c = a * b;
-printf("%d\n", c);
-return (0);
+	if (argc != 3)
+	{
+		puts("Error");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+	printf("%d\n", c);
+	return (0);
 }
