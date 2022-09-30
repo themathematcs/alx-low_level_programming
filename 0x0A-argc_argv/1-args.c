2f510d02a0b0ad3305d1bc8fbaa28026638833ca
themathematcs <chriznganga69@gmail.com>
2022-09-30 08:01:34 -0700

    second quiz

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints no: of args
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[] _attribute_((unused)))
+{
+printf("%d\n", argc - 1);
+
+return (0);
+}
