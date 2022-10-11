5f136be0fef85a865039fbbc41722a08c80a2c47
themathematcs <chriznganga69@gmail.com>
2022-10-11 12:29:18 -0700

    gogs huh!

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,7 +22,7 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *, float, char *);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *scr);
 int _strlen(char *s);
