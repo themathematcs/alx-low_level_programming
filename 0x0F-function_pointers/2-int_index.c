484b1d0b3ee13db01381d5bd1e7d23d112e98bca
themathematcs <chriznganga69@gmail.com>
2022-10-13 17:15:12 -0700

    Hot fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 int i;
 
-if (array && cmp)
-{
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if (cmp(array[i] != 0))
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
 	}
-}
-return (-1);
+	return (-1);
 }
